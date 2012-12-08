c6d32236e48a1352b0caeffb8d84938f98b39a11
Eric Christiansen <ericmartinchristiansen@gmail.com>
2012-12-07 21:31:42 -0800

    small bug fix

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -57,8 +57,7 @@ object $name$Build extends Build {
     assemblySettings
 
   lazy val root = {
-    val name = "$name$"
-    val settings = libSettings ++ Seq(name := name)
-    Project(id = name, base = file("."), settings = settings)
+    val settings = libSettings ++ Seq(name := "$name$")
+    Project(id = "$name$", base = file("."), settings = settings)
   }
 }
