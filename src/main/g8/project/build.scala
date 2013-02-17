e2387206df8343f05f79528d57d7e16283aeac8d
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-02-17 12:32:26 -0800

    did stuff

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -9,7 +9,7 @@ object $name$Build extends Build {
     resolvers ++= Seq(
       "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
       "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-//      "repo.codahale.com" at "http://repo.codahale.com",
+//      "repo.codahale.com" at "http://repo.codahale.com",                                                                                                                                   
       "spray-io" at "http://repo.spray.io/",
       "typesafe-releases" at "http://repo.typesafe.com/typesafe/repo"
     )
@@ -17,20 +17,17 @@ object $name$Build extends Build {
 
   def extraLibraryDependencies = Seq(
     libraryDependencies ++= Seq(
-      "org.apache.commons" % "commons-math3" % "3.0",
+      "org.apache.commons" % "commons-math3" % "3.1.1",
       "commons-io" % "commons-io" % "2.4",
-      "com.frugalmechanic" % "scala-optparse" % "1.1",
-      "org.scalatest" %% "scalatest" % "2.0.M5" % "test",
+      "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
       "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
       "org.scala-tools" %% "scala-stm" % "0.6",
-      "com.twitter" % "util-eval" % "6.0.1",
       "com.chuusai" %% "shapeless" % "1.2.3",
-      "org.clapper" %% "grizzled-scala" % "1.0.13",
+      "org.clapper" %% "grizzled-scala" % "1.1.3",
       "org.scalanlp" %% "breeze-math" % "0.1",
       "org.scalaz" %% "scalaz-core" % "7.0-SNAPSHOT",
       "io.spray" %% "spray-json" % "1.2.2" cross CrossVersion.full,
       "junit" % "junit" % "4.11" % "test",
-      "org.spark-project" % "spark-core_2.9.2" % "0.6.1",
       "org.imgscalr" % "imgscalr-lib" % "4.2"
     )
   )
@@ -40,23 +37,24 @@ object $name$Build extends Build {
     scalacOptions ++= Seq(
       "-optimize",
       "-unchecked",
-      "-deprecation"
+      "-deprecation",
       "-feature",
       "-language:implicitConversions",
-      // "-language:reflectiveCalls",
+      // "-language:reflectiveCalls",                                                                                                                                                        
       "-language:postfixOps"
     )
   )
 
-  def libSettings = 
-    Project.defaultSettings ++ 
-    extraResolvers ++ 
-    extraLibraryDependencies ++ 
-    scalaSettings ++ 
+  def libSettings =
+    Project.defaultSettings ++
+    extraResolvers ++
+    extraLibraryDependencies ++
+    scalaSettings ++
     assemblySettings
 
+  val projectName = "$name$"
   lazy val root = {
-    val settings = libSettings ++ Seq(name := "$name$")
-    Project(id = "$name$", base = file("."), settings = settings)
+    val settings = libSettings ++ Seq(name := projectName)
+    Project(id = projectName, base = file("."), settings = settings)
   }
 }
