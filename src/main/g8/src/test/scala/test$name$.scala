7996bebaab5f2602aa42a702935bfb03887cde55
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-07-21 16:58:22 -0700

    scalatest-extra dependency

--- src/main/g8/src/test/scala/test$name$.scala
+++ src/main/g8/src/test/scala/test$name$.scala
@@ -4,6 +4,7 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalacheck.Gen
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import scalatestextra._
 
 ////////////////////////////////////////////////////////////////////////////////
 
